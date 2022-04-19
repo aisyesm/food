d1bcbb5b5b9dca56931ce92501f6618a9795a7d7
Ais <aisyesm@mail.ru>
2022-04-19 18:46:09 -0400

    Implement modal behavior

--- src/js/main.js
+++ src/js/main.js
@@ -101,4 +101,51 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline); 
+
+
+    // Modal
+
+    const openModalBtns = document.querySelectorAll('[data-modal]'),
+          closeModalBtn = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
+
+    function closeModal() {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
+    function openModal() {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    openModalBtns.forEach(el => {
+        el.addEventListener('click', openModal);
+    });
+
+    closeModalBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.style.display === 'block') {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
